b1f582109a4e3930147cfe2f36e952b987ed4610
Alejandro Garcia <agarciacoding@gmail.com>
2024-03-25 20:47:11 -0500

    small updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,17 +13,17 @@ const Home = () => {
           <Container className="text-center text-white text-content">
             <Row className="justify-content-center align-items-center">
               <Col>
-                <h1 className="display-4 mb-4">Welcome to Where To Watch</h1>
+                <h1 className="display-4 mb-4">Your streaming guide for movies & TV shows</h1>
                 <p className="lead mb-5">Discover and watch your favorite movies and TV shows</p>
                 <Button color="primary" size="lg">Get Started</Button>
               </Col>
             </Row>
-            <Row className="mt-5">
+            {/* <Row className="mt-5">
               <Col>
                 <h2 className="mb-4">Popular Movies</h2>
-                {/* Add code to display random movies here */}
+                { Add code to display random movies here }
               </Col>
-            </Row>
+            </Row> */}
           </Container>
           
         </div>
